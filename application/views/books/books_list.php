a1da92dc1c8e1e4f20744d2039f7b643c07a949a
Ruang Programmer <ruangprogramer@gmail.com>
2017-08-24 15:47:29 +0700

    Update books_list.php

--- application/views/books/books_list.php
+++ application/views/books/books_list.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
     <head>
-        <title>harviacode.com - codeigniter crud generator</title>
+        <title>www.ruangprogrammer.com</title>
         <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
         <style>
             body{
